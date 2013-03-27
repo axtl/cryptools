84d74cbe4c686879fd0160a4c7cbff350937f617
alexandru <alex@hackd.net>
2013-03-27 05:01:08 -0700

    docstrings

--- cryptools/stringutils/convert.py
+++ cryptools/stringutils/convert.py
@@ -1,27 +1,67 @@
-"""This module contains a few utility function that work around Python's
-formatting of various types.
+# -*- coding: utf-8 -*-
+"""This module contains a few conversion utility functions for that work
+around Python's formatting of various data types.
 """
 import math
 
 
-def htoi(n):
-    """Convert a hexit to its integer value."""
-    return eval(bin(ord(n)))
+def htoi(h):
+    """Convert a hexit to its integer value.
+
+    >>> htoi('\\x74')
+    116
+
+    :param h: hexit to convert
+    :return: integer value representation
+    """
+    return eval(bin(ord(h)))
 
 
 def itoh(i):
-    """Convert integer to hexit of the form \x00."""
+    """Convert integer to hexit of the form ``\\x00``.
+
+
+    Normally, Python's built-in :func:`hex` function will return ``0x00``, even
+    though that wouldn't work if we were directly trying to hex decode it.
+
+    >>> itoh(116)
+    '\\x74'
+
+    :param i: integer value to convert
+    :return: hexit of the form ``\\x00``
+    """
     return '\\' + hex(i)[1:]
 
 
 def htoc(h):
-    """Convert hexit to character."""
+    """Convert hexit to character.
+
+    This function does not check if the character is printable or not.
+
+    >>> htoc('\\x74')
+    't'
+
+    :param h: hexit to convert
+    :return: character representation of the hexit
+    """
     return h[-2:].decode('hex')
 
 
 def xor(str1, str2):
-    """XOR two strings together. The shorter string will be padded with itself
-    in order to match the length of the longer one.
+    """XOR two strings.
+
+
+    If the strings are not equal, the shorter one will be padded with itself,
+    repeating, to match the length of the longer one.
+
+    >>> xor('HAL', 'IBM')
+    '\\x01\\x03\\x01'
+    >>> xor('foo', 'foobar')
+    '\\x00\\x00\\x00\\x04\\x03\\x1d'
+
+    :param str1: first string to use as input
+    :param str2: second string to use as input
+    :return: a string resulting from XOR-ing the two input parameters
     """
     # sort by length
     in1, in2 = sorted([str1, str2], key=len)
@@ -35,5 +75,18 @@ def xor(str1, str2):
 
 
 def xor_hex(buff1, buff2):
-    """Return the hex-encoded value of XOR-ing two (hex-encoded) buffers"""
+    """Return the hex-encoded value of XOR-ing two (hex-encoded) buffers.
+
+    The buffers are hex-decoded, passed to :func:`xor`, and the result is
+    hex-encoded before being returned.
+
+    >>> h1 = "686974207468652062756c6c277320657965"
+    >>> h2 = "746865206b696420646f6e277420706c6179"
+    >>> xor(h1, h2)
+    '1c0111001f010100061a024b53535009181c'
+
+    :param buff1: first hex-encoded buffer
+    :param buff2: second hex-encoded buffer
+    :return: a hex-encoded string resulting from XOR-ing the two buffers
+    """
     return xor(buff1.decode('hex'), buff2.decode('hex')).encode('hex')
