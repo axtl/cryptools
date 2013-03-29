9cb8f14c82963a4b91be7b26b361c3ca562a0561
alexandru <alex@hackd.net>
2013-03-28 17:49:05 -0700

    doc{string|test}s

--- cryptools/stringutils/freq.py
+++ cryptools/stringutils/freq.py
@@ -4,17 +4,35 @@ from collections import defaultdict, Counter
 
 
 def char_count(text):
+    """Counts occurrences of each (printable) character in the input.
+
+    The count is case-sensitive.
+
+    >>> char_count("to be or not to be.")
+    Counter({' ': 5, 'o': 4, 't': 3, 'b': 2, 'e': 2, 'n': 1, 'r': 1, '.': 1})
+
+    :param text: the text to analyze
+    :return: a ``collections.Counter`` with the counts for each character
+    """
     freqs = Counter()
     freqs.update(text)
     # trim to useful characters
     for ch in freqs.keys():
-        if ch not in (string.letters + string.digits + string.whitespace):
+        if ch not in (string.printable):
             freqs.pop(ch)
     return freqs
 
 
 # total numbers
 def get_freqs(counter):
+    """Computes the relative frequencies for elements in the provided counter.
+
+    >>> get_freqs(Counter('test'))
+    defaultdict(<type 'int'>, {'s': 0.25, 'e': 0.25, 't': 0.5})
+
+    :param counter: the input counter to compute frequencies for
+    :return: a ``collections.defaultdict`` with the appropriate frequencies
+    """
     elems = float(sum(counter.values()))
     nfreqs = defaultdict(int)
     for el in counter:
