0f004830bd220ad8da1d4b151897630431d2f195
alexandru <alex@hackd.net>
2013-04-22 04:25:24 -0700

    tweak scoring functions, always

--- cryptools/crack.py
+++ cryptools/crack.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import math
 import string
 
 from stringutils import convert, freq
@@ -12,7 +13,7 @@ def brute_xor(cyphertext, st_freqs):
 
     :param cyphertext: the cyphertext to crack
     :param st_freqs: a Counter of standard frequencies in the target language
-    :return: ``(key, message, distance)``
+    :return: ``(key, message, score)``
     """
     # standard frequency counts
     st_keys = st_freqs.keys()
@@ -28,18 +29,20 @@ def brute_xor(cyphertext, st_freqs):
         dec = convert.xor(test, cyphertext)
         cand_freqs = freq.get_freqs(freq.char_count(dec.lower()))
         cand_keys = cand_freqs.keys()
-        distance = 0.0
+        score = 0.0
         for c in cand_freqs:
-            # use two classifiers, based on pos'n in std freq list & freq dist
+            # scoring
             try:
                 st_in = st_keys.index(c)
             except ValueError:
                 st_in = st_len
-            distance += abs(cand_keys.index(c) - st_in)
-            distance += abs(st_freqs[c] - cand_freqs[c]) * 100
 
-        if lowdist > distance:
-            lowdist = distance
+            # find better scoring functions
+            score += abs(cand_keys.index(c) - st_in)
+            score += abs(st_freqs[c] - cand_freqs[c]) * 100
+
+        if lowdist > score:
+            lowdist = score
             topchoice = dec
             key = test
 
